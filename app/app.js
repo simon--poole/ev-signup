8f0bda539603131a05b1bef006d10c25412db094
Simon Poole <simon__poole@hotmail.com>
2016-06-30 22:58:32 +0100

    Email validation & confirmation

--- app/app.js
+++ app/app.js
@@ -1,30 +1,57 @@
-var express = require( 'express' )();
-var mongoose = require( 'mongoose' );
-var bodyparser = require( 'body-parser' )
-var Signups = require( './signup_model.js' );
-express.use( bodyparser.urlencoded( {
+var express = require('express')();
+var helper = require('sendgrid').mail;
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser')
+var Signups = require('./signup_model.js');
+express.use(bodyparser.urlencoded({
     extended: true
-} ) )
-express.use( bodyparser.json() );
-express.use( function( req, res, next ) {
-    res.header( "Access-Control-Allow-Origin", req.protocol + "://eventvods.com" );
-    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+}))
+express.use(bodyparser.json());
+express.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.protocol + "://eventvods.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-} );
-express.post( '/signup', function( req, res ) {
-    Signups.validate( req.body )
-        .then( function() {
-            Signups.create( req.body, function( err, data ) {
-                if ( err ) res.sendStatus( '500' );
-                else res.sendStatus( '200' );
-            } );
-        } )
-        .catch( function( errs ) {
-            res.sendStatus( '400' );
-        } )
-} );
-express.listen( process.env.PORT );
-mongoose.connect( process.env.DB );
-mongoose.connection.on( 'error', ( err ) => {
-    console.log( err );
-} );
+});
+express.post('/signup', function(req, res) {
+    Signups.validate(req.body)
+        .then(function() {
+            Signups.create(req.body, function(err, doc) {
+                if (err) res.sendStatus('500');
+                else Signups.email(doc)
+                    .then(function(){
+                        res.sendStatus('200');
+                    })
+                    .catch(function(){
+                        res.sendStatus('500');
+                    })
+            });
+        })
+        .catch(function(errs) {
+            res.sendStatus('400');
+        });
+});
+express.get('/verify/:id/:code', function(req, res) {
+    Signups.findById(req.params.id, function(err, doc){
+        if (err) res.sendStatus('500');
+        if(req.params.code === doc.code){
+            doc.confirmed = true;
+            doc.save(function(err){
+                if(err) res.sendStatus('500');
+                else res.redirect('http://eventvods.com/beta/complete');
+            });
+        }
+        else res.send('Invalid verification code. Please double check your email for the correct link.');
+    });
+});
+express.get('/test/', function(req, res){
+    Signups.email({
+        "email": "simon__poole@hotmail.com",
+        "_id": "testing_id",
+        "code": "codetest"
+    });
+});
+express.listen(process.env.PORT);
+mongoose.connect(process.env.DB);
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
--- app/signup_model.js
+++ app/signup_model.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 var Indicative = require('indicative');
 var shortid = require('shortid');
+var helper = require('sendgrid').mail;
+var sg = require('sendgrid').SendGrid(process.env.SENDGRID);
+var q = require('q');
 var signupSchema = mongoose.Schema({
     email: {
-    	type: String,
-    	required: true
+        type: String,
+        required: true
     },
     confirmed: {
         type: Boolean,
@@ -15,19 +18,55 @@ var signupSchema = mongoose.Schema({
         default: shortid.generate()
     },
     timestamp: {
-   		type: Date,
-   		default: Date.now
+        type: Date,
+        default: Date.now
     },
     comments: String
 });
 
 var validator = {
-  email: 'required|email',
-  comments: 'max:500'
-}
+    email: 'required|email',
+    comments: 'max:500'
+};
 
 module.exports = mongoose.model('Signup', signupSchema);
 
-module.exports.validate = function(data){
-	return Indicative.validateAll(data, validator);
-}
+module.exports.validate = function(data) {
+    return Indicative.validateAll(data, validator);
+};
+module.exports.email = function(doc) {
+    var promise = q.defer();
+    var from, to, subject, html, plain, mail, json, request, verifyemail, personalization, url, tracking;
+    from = new helper.Email("no-reply@mailer.eventvods.com", "Eventvods");
+    to = new helper.Email(doc.email);
+    subject = "Eventvods.com Email Confirmation";
+    html = new helper.Content("text/html", "Eventvods");
+    plain = new helper.Content("text/plain", "Eventvods");
+    mail = new helper.Mail();
+    mail.setTemplateId("0a91c12e-0a74-4608-bd09-8e65c0fc3508");//confirmation email
+    url = "http://ev-signup.herokuapp.com/verify/" + doc._id + "/" + doc.code;
+    verifyemail = new helper.Substitution("-verifyurl-", url);
+    tracking = new helper.ClickTracking(true, true);
+    personalization = new helper.Personalization();
+    personalization.addSubstitution(verifyemail);
+    personalization.substitutions = personalization.substitions;
+    personalization.addTo(to);
+    mail.setSubject(subject);
+    mail.setFrom(from);
+    mail.addPersonalization(personalization);
+    mail.addContent(plain);
+    mail.addContent(html);
+    mail.addTrackingSettings(tracking);
+    json = mail.toJSON();
+    request = sg.emptyRequest();
+    request.method = 'POST';
+    request.path = '/v3/mail/send';
+    request.body = json;
+    sg.API(request, function(response) {
+        if(response.statusCode === 202)
+            promise.resolve();
+        else
+            promise.reject();
+    });
+    return promise.promise;
+};
